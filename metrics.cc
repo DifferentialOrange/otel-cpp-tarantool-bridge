fadfb1d7a9d35d01677ff49a872c2f7322cb7a43
Georgy Moiseev <georgy.moiseev@corp.mail.ru>
2023-04-10 11:11:28 +0300

    app: api to setup a collector
    This patch finishes the prototype. See README.md for more detailed info.

--- metrics.cc
+++ metrics.cc
@@ -1,6 +1,3 @@
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
-
 #include <memory>
 #include <thread>
 #include "opentelemetry/exporters/otlp/otlp_http_metric_exporter.h"
@@ -11,6 +8,8 @@
 #include "opentelemetry/sdk/metrics/export/periodic_exporting_metric_reader.h"
 #include "opentelemetry/sdk/metrics/meter.h"
 #include "opentelemetry/sdk/metrics/meter_provider.h"
+#include "opentelemetry/metrics/async_instruments.h"
+#include "opentelemetry/metrics/sync_instruments.h"
 
 #include "examples/common/metrics_foo_library/foo_library.h"
 
@@ -18,120 +17,173 @@ namespace metric_sdk      = opentelemetry::sdk::metrics;
 namespace common          = opentelemetry::common;
 namespace exportermetrics = opentelemetry::exporter::otlp;
 namespace metrics_api     = opentelemetry::metrics;
+namespace nostd       = opentelemetry::nostd;
 
 #include <iostream>
 #include <iterator>
 
 extern "C" {
 #include <lauxlib.h>
+#include <lualib.h>
 #include <lua.h>
+#include <luaconf.h>
+
+#include <tarantool/module.h>
 }  // extern "C"
 
-namespace
+extern "C" {
+static const char otlp_provider_mt_name[] = "__tnt_otlp_provider";
+static const char otlp_counter_mt_name[] = "__tnt_otlp_counter";
+}
+
+static int lua_create_provider(struct lua_State *L)
 {
+	metric_sdk::MeterProvider** provider_ptr = (metric_sdk::MeterProvider **) lua_newuserdata(L, sizeof(metric_sdk::MeterProvider*));
+	
+	auto shared_p = std::shared_ptr<metrics_api::MeterProvider>(new metric_sdk::MeterProvider());
+    metrics_api::Provider::SetMeterProvider(shared_p);
 
-void InitMetrics(const std::string &name)
+    *provider_ptr = std::static_pointer_cast<metric_sdk::MeterProvider>(shared_p).get();
+
+	luaL_getmetatable(L, otlp_provider_mt_name);
+	lua_setmetatable(L, -2);
+	return 1;
+}
+
+static int lua_clean_provider(struct lua_State *L)
 {
-  exportermetrics::OtlpHttpMetricExporterOptions exporter_options;
-  auto exporter = exportermetrics::OtlpHttpMetricExporterFactory::Create(exporter_options);
+	metric_sdk::MeterProvider** provider_ptr = (metric_sdk::MeterProvider **) luaL_checkudata(L, 1, otlp_provider_mt_name);
 
-  std::string version{"1.2.0"};
-  std::string schema{"https://opentelemetry.io/schemas/1.2.0"};
+	if (*provider_ptr == NULL) {
+		return 0;
+	}
 
-  metric_sdk::PeriodicExportingMetricReaderOptions options;
-  options.export_interval_millis = std::chrono::milliseconds(1000);
-  options.export_timeout_millis  = std::chrono::milliseconds(500);
-  std::unique_ptr<metric_sdk::MetricReader> reader{
-      new metric_sdk::PeriodicExportingMetricReader(std::move(exporter), options)};
-  auto provider = std::shared_ptr<metrics_api::MeterProvider>(new metric_sdk::MeterProvider());
-  auto p        = std::static_pointer_cast<metric_sdk::MeterProvider>(provider);
-  p->AddMetricReader(std::move(reader));
+	std::shared_ptr<metrics_api::MeterProvider> none;
+	metrics_api::Provider::SetMeterProvider(none);
 
-  // counter view
-  std::string counter_name = name + "_counter";
-  std::unique_ptr<metric_sdk::InstrumentSelector> instrument_selector{
-      new metric_sdk::InstrumentSelector(metric_sdk::InstrumentType::kCounter, counter_name)};
-  std::unique_ptr<metric_sdk::MeterSelector> meter_selector{
-      new metric_sdk::MeterSelector(name, version, schema)};
-  std::unique_ptr<metric_sdk::View> sum_view{
+	delete *provider_ptr;
+
+	*provider_ptr = NULL;
+
+	return 0;
+}
+
+static int lua_provider_init_otlp_http_exporter(struct lua_State *L)
+{
+	metric_sdk::MeterProvider** provider_ptr = (metric_sdk::MeterProvider **) luaL_checkudata(L, 1, otlp_provider_mt_name);
+	std::string url = luaL_checkstring(L, 2);
+
+	exportermetrics::OtlpHttpMetricExporterOptions exporter_options;
+	exporter_options.url = url;
+	auto exporter = exportermetrics::OtlpHttpMetricExporterFactory::Create(exporter_options);
+
+	metric_sdk::PeriodicExportingMetricReaderOptions options;
+	options.export_interval_millis = std::chrono::milliseconds(1000);
+	options.export_timeout_millis  = std::chrono::milliseconds(500);
+	std::unique_ptr<metric_sdk::MetricReader> reader{
+	    new metric_sdk::PeriodicExportingMetricReader(std::move(exporter), options)};
+
+	(*provider_ptr)->AddMetricReader(std::move(reader));
+
+	return 0;
+}
+
+static int lua_provider_new_counter(struct lua_State *L)
+{
+	metric_sdk::MeterProvider** provider_ptr = (metric_sdk::MeterProvider **) luaL_checkudata(L, 1, otlp_provider_mt_name);
+	std::string name = luaL_checkstring(L, 2);
+
+	std::string version{"1.2.0"};
+	std::string schema{"https://opentelemetry.io/schemas/1.2.0"};
+
+  	std::string counter_name = name + "_counter";
+  	std::unique_ptr<metric_sdk::InstrumentSelector> instrument_selector{
+      	new metric_sdk::InstrumentSelector(metric_sdk::InstrumentType::kCounter, counter_name)};
+  	std::unique_ptr<metric_sdk::MeterSelector> meter_selector{
+      	new metric_sdk::MeterSelector(name, version, schema)};
+  	std::unique_ptr<metric_sdk::View> sum_view{
       new metric_sdk::View{name, "description", metric_sdk::AggregationType::kSum}};
-  p->AddView(std::move(instrument_selector), std::move(meter_selector), std::move(sum_view));
 
-  // observable counter view
-  std::string observable_counter_name = name + "_observable_counter";
-  std::unique_ptr<metric_sdk::InstrumentSelector> observable_instrument_selector{
-      new metric_sdk::InstrumentSelector(metric_sdk::InstrumentType::kObservableCounter,
-                                         observable_counter_name)};
-  std::unique_ptr<metric_sdk::MeterSelector> observable_meter_selector{
-      new metric_sdk::MeterSelector(name, version, schema)};
-  std::unique_ptr<metric_sdk::View> observable_sum_view{
-      new metric_sdk::View{name, "test_description", metric_sdk::AggregationType::kSum}};
-  p->AddView(std::move(observable_instrument_selector), std::move(observable_meter_selector),
-             std::move(observable_sum_view));
+	(*provider_ptr)->AddView(std::move(instrument_selector), std::move(meter_selector), std::move(sum_view));
 
-  // histogram view
-  std::string histogram_name = name + "_histogram";
-  std::unique_ptr<metric_sdk::InstrumentSelector> histogram_instrument_selector{
-      new metric_sdk::InstrumentSelector(metric_sdk::InstrumentType::kHistogram, histogram_name)};
-  std::unique_ptr<metric_sdk::MeterSelector> histogram_meter_selector{
-      new metric_sdk::MeterSelector(name, version, schema)};
-  std::shared_ptr<opentelemetry::sdk::metrics::AggregationConfig> aggregation_config{
-      new opentelemetry::sdk::metrics::HistogramAggregationConfig};
-  static_cast<opentelemetry::sdk::metrics::HistogramAggregationConfig *>(aggregation_config.get())
-      ->boundaries_ = std::vector<double>{0.0,    50.0,   100.0,  250.0,   500.0,  750.0,
-                                          1000.0, 2500.0, 5000.0, 10000.0, 20000.0};
-  std::unique_ptr<metric_sdk::View> histogram_view{new metric_sdk::View{
-      name, "description", metric_sdk::AggregationType::kHistogram, aggregation_config}};
-  p->AddView(std::move(histogram_instrument_selector), std::move(histogram_meter_selector),
-             std::move(histogram_view));
-  metrics_api::Provider::SetMeterProvider(provider);
+	nostd::shared_ptr<metrics_api::Meter> meter = (*provider_ptr)->GetMeter(name, version);
+	nostd::unique_ptr<metrics_api::Counter<double>> double_counter = meter->CreateDoubleCounter(counter_name);
+
+	metrics_api::Counter<double>** counter_ptr = (metrics_api::Counter<double> **) lua_newuserdata(L, sizeof(metrics_api::Counter<double>*));
+	*counter_ptr = double_counter.release();
+
+	luaL_getmetatable(L, otlp_counter_mt_name);
+	lua_setmetatable(L, -2);
+
+	return 1;
 }
 
-void CleanupMetrics()
+static int lua_counter_add(struct lua_State *L)
 {
-  std::shared_ptr<metrics_api::MeterProvider> none;
-  metrics_api::Provider::SetMeterProvider(none);
+	metrics_api::Counter<double>** counter_ptr = (metrics_api::Counter<double> **) luaL_checkudata(L, 1, otlp_counter_mt_name);
+
+	luaL_checktype(L, 2, LUA_TNUMBER);
+	double val = lua_tonumber(L, 2);
+
+	(*counter_ptr)->Add(val);
+
+	return 0;
 }
-}  // namespace
 
-static int run(lua_State* L) noexcept
-{
-  std::string name{"metric_example"};
-  InitMetrics(name);
-
-  std::thread counter_example{&foo_library::counter_example, name};
-  std::thread observable_counter_example{&foo_library::observable_counter_example, name};
-  std::thread histogram_example{&foo_library::histogram_example, name};
-
-  counter_example.join();
-  observable_counter_example.join();
-  histogram_example.join();
-
-  CleanupMetrics();
-
-  return 1;
-}
 
 // Copied from Lua 5.3 so that we can use it with Lua 5.1.
 static void setfuncs(lua_State* L, const luaL_Reg* l, int nup) {
-  luaL_checkstack(L, nup + 1, "too many upvalues");
-  for (; l->name != NULL; l++) { /* fill the table with given functions */
-    int i;
-    lua_pushstring(L, l->name);
-    for (i = 0; i < nup; i++) /* copy upvalues to the top */
-      lua_pushvalue(L, -(nup + 1));
-    lua_pushcclosure(L, l->func, nup); /* closure with those upvalues */
-    lua_settable(L, -(nup + 3));
-  }
-  lua_pop(L, nup); /* remove upvalues */
+	luaL_checkstack(L, nup + 1, "too many upvalues");
+	for (; l->name != NULL; l++) { /* fill the table with given functions */
+		int i;
+		lua_pushstring(L, l->name);
+		for (i = 0; i < nup; i++) /* copy upvalues to the top */
+			lua_pushvalue(L, -(nup + 1));
+		lua_pushcclosure(L, l->func, nup); /* closure with those upvalues */
+		lua_settable(L, -(nup + 3));
+	}
+	lua_pop(L, nup); /* remove upvalues */
 }
 
-extern "C" int luaopen_metrics(lua_State* L) {
-  lua_newtable(L);
-  const struct luaL_Reg functions[] = {
-      {"run", run},
-      {nullptr, nullptr}};
-  setfuncs(L, functions, 0);
+extern "C" int luaopen_metrics(struct lua_State *L)
+{
+	lua_newtable(L);
 
-  return 1;
+	static const struct luaL_Reg otlp_provider_methods [] = {
+		{"init_otlp_http_exporter", lua_provider_init_otlp_http_exporter},
+		{"new_counter", lua_provider_new_counter},
+		{"__gc", lua_clean_provider},
+		{NULL, NULL}
+	};
+	luaL_newmetatable(L, otlp_provider_mt_name);
+	lua_pushvalue(L, -1);
+	setfuncs(L, otlp_provider_methods, 0);
+	lua_setfield(L, -2, "__index");
+	lua_pushstring(L, otlp_provider_mt_name);
+	lua_setfield(L, -2, "__metatable");
+	lua_pop(L, 1);
+
+	lua_newtable(L);
+
+	static const struct luaL_Reg otlp_counter_methods [] = {
+		{"add", lua_counter_add},
+		{NULL, NULL}
+	};
+	luaL_newmetatable(L, otlp_counter_mt_name);
+	lua_pushvalue(L, -1);
+	setfuncs(L, otlp_counter_methods, 0);
+	lua_setfield(L, -2, "__index");
+	lua_pushstring(L, otlp_counter_mt_name);
+	lua_setfield(L, -2, "__metatable");
+	lua_pop(L, 1);
+
+	lua_newtable(L);
+
+	static const struct luaL_Reg meta [] = {
+		{"new", lua_create_provider},
+		{NULL, NULL}
+	};
+	setfuncs(L, meta, 0);
+
+	return 1;
 }
