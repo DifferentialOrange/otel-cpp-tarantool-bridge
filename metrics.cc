21d786138bf16002fda047617d2e66bdc63ed56c
Georgy Moiseev <georgy.moiseev@corp.mail.ru>
2023-04-21 17:21:37 +0300

    api: support export to Lua

--- metrics.cc
+++ metrics.cc
@@ -4,6 +4,7 @@
 #include <random>
 #include <algorithm>
 #include <vector>
+#include <variant>
 #include "opentelemetry/exporters/otlp/otlp_http_metric_exporter.h"
 #include "opentelemetry/exporters/otlp/otlp_http_metric_exporter_factory.h"
 #include "opentelemetry/metrics/provider.h"
@@ -39,6 +40,7 @@ extern "C" {
 static const char otlp_provider_mt_name[] = "__tnt_otlp_provider";
 static const char otlp_meter_mt_name[] = "__tnt_otlp_meter";
 static const char otlp_double_counter_mt_name[] = "__tnt_otlp_double_counter";
+static const char otlp_reader_mt_name[] = "__tnt_otlp_reader";
 }
 
 static int lua_create_provider(struct lua_State *L)
@@ -91,6 +93,229 @@ static int lua_provider_init_otlp_http_exporter(struct lua_State *L)
 	(*provider_ptr)->AddMetricReader(std::move(reader));
 
 	return 0;
+}
+
+class MyExporter : public metric_sdk::MetricReader
+{
+public:
+  /**
+   * Constructor - binds an exposer and collector to the exporter
+   * @param options: options for an exposer that exposes
+   *  an HTTP endpoint for the exporter to connect to
+   */
+  MyExporter();
+
+  metric_sdk::AggregationTemporality GetAggregationTemporality(
+      metric_sdk::InstrumentType instrument_type) const noexcept override;
+
+private:
+  bool OnForceFlush(std::chrono::microseconds timeout) noexcept override;
+
+  bool OnShutDown(std::chrono::microseconds timeout) noexcept override;
+
+  void OnInitialized() noexcept override;
+};
+
+MyExporter::MyExporter()
+{
+
+}
+
+metric_sdk::AggregationTemporality MyExporter::GetAggregationTemporality(
+    metric_sdk::InstrumentType /* instrument_type */) const noexcept
+{
+  // Prometheus exporter only support Cumulative
+  return metric_sdk::AggregationTemporality::kCumulative;
+}
+
+bool MyExporter::OnForceFlush(std::chrono::microseconds /* timeout */) noexcept
+{
+  return true;
+}
+
+bool MyExporter::OnShutDown(std::chrono::microseconds /* timeout */) noexcept
+{
+  return true;
+}
+
+void MyExporter::OnInitialized() noexcept {}
+
+
+static int lua_provider_new_reader(struct lua_State *L)
+{
+	metric_sdk::MeterProvider** provider_ptr = (metric_sdk::MeterProvider **) luaL_checkudata(L, 1, otlp_provider_mt_name);
+
+	auto reader = std::shared_ptr<metric_sdk::MetricReader>(new MyExporter);
+	
+	(*provider_ptr)->AddMetricReader(reader);
+
+	metric_sdk::MetricReader** reader_ptr = (metric_sdk::MetricReader **) lua_newuserdata(L, sizeof(metric_sdk::MetricReader*));
+	*reader_ptr = reader.get();
+
+	luaL_getmetatable(L, otlp_reader_mt_name);
+	lua_setmetatable(L, -2);
+
+	return 1;
+}
+
+static int lua_reader_collect(struct lua_State *L)
+{
+	metric_sdk::MetricReader** reader_ptr = (metric_sdk::MetricReader **) luaL_checkudata(L, 1, otlp_reader_mt_name);
+
+	(*reader_ptr)->Collect([L](metric_sdk::ResourceMetrics &metric_data) {
+		lua_newtable(L); // return value
+
+		lua_newtable(L); // "resourceMetrics" array
+
+		lua_pushnumber(L, 1); // "resourceMetrics" array key
+		lua_newtable(L); // "resourceMetrics" array element
+
+		lua_newtable(L); // "resource" field of "resourceMetrics" array element
+		auto attributes = metric_data.resource_->GetAttributes();
+
+		lua_newtable(L); // "attributes" field of "resource" map
+		int i = 0;
+		for (auto const& attr : attributes) {
+			i++;
+			lua_pushnumber(L, i); // "attributes" array key
+
+			lua_newtable(L); // "attributes" array element
+
+			lua_pushstring(L, attr.first.c_str()); // "key" field value of "attributes" array element
+			lua_setfield(L, -2, "key"); // push "key" field to "attributes" array element
+
+			lua_newtable(L);  // "value" field value of "attributes" array element
+
+			if (nostd::holds_alternative<std::string>(attr.second)) {
+				auto strval = nostd::get<std::string>(attr.second);
+				lua_pushstring(L, strval.c_str()); // "stringValue" field value of "value" map
+				lua_setfield(L, -2, "stringValue"); // push "stringValue" field to "value" map
+			}
+
+			lua_setfield(L, -2, "value"); // push "value" field to "attributes" array element
+
+			lua_settable(L, -3); // push "attributes" array element to "attributes" array
+		}
+		lua_setfield(L, -2, "attributes"); // push "attributes" field to "resource" map
+
+		lua_setfield(L, -2, "resource"); // push "resource" field to "resourceMetrics" array element
+
+		lua_newtable(L); // "scopeMetrics" field of "resourceMetrics" array element
+
+		int j = 0;
+		for (auto const& scope : metric_data.scope_metric_data_) {
+			j++;
+			lua_pushnumber(L, j); // "scopeMetrics" array key
+
+			lua_newtable(L); // "scopeMetrics" array element
+
+			lua_newtable(L); // "scope" field of "scopeMetrics" array element
+
+			lua_pushstring(L, scope.scope_->GetName().c_str()); // "name" field of "scope" map
+			lua_setfield(L, -2, "name"); // "name" field of "scope" map
+
+			lua_pushstring(L, scope.scope_->GetVersion().c_str()); // "version" field of "scope" map
+			lua_setfield(L, -2, "version"); // "version" field of "scope" map
+
+			// schemaURL is also should be here
+
+			lua_setfield(L, -2, "scope"); // push "scope" field to "scopeMetrics" array element
+
+			lua_newtable(L); // "metrics" field of "scopeMetrics" array element
+
+			int k = 0;
+			for (auto const& metric : scope.metric_data_) {
+				k++;
+				lua_pushnumber(L, k); // "metrics" array key
+
+				lua_newtable(L); // "metrics" array element
+
+				lua_pushstring(L, metric.instrument_descriptor.name_.c_str()); // "name" field of "metrics" array element
+				lua_setfield(L, -2, "name"); // push "name" field of "metrics" array element
+
+				lua_newtable(L); // "sum" field of "metrics" array element
+
+				lua_pushnumber(L, int(metric.aggregation_temporality)); // "aggregationTemporality" field of "sum" map
+				lua_setfield(L, -2, "aggregationTemporality"); // push "aggregationTemporality" field of "sum" map
+
+				lua_pushboolean(L, true); // "isMonotonic" field of "sum" map
+				lua_setfield(L, -2, "isMonotonic"); // push "isMonotonic" field of "sum" map
+
+				lua_newtable(L); // "dataPoints" field of "sum" map
+
+				int l = 0;
+				for (auto const& point : metric.point_data_attr_) {
+					l++;
+					lua_pushnumber(L, l); // "dataPoints" array key
+
+					lua_newtable(L); // "dataPoints" array element
+
+					if (!point.attributes.empty()) {
+						lua_newtable(L); // "attributes" field of "dataPoints" array element
+
+						int m = 0;
+						for (auto const& attr : point.attributes) {
+							m++;
+							lua_pushnumber(L, m); // "attributes" array key
+
+							lua_newtable(L); // "attributes" array element
+
+							lua_pushstring(L, attr.first.c_str()); // "key" field value of "attributes" array element
+							lua_setfield(L, -2, "key"); // push "key" field to "attributes" array element
+
+							lua_newtable(L);  // "value" field value of "attributes" array element
+
+							if (nostd::holds_alternative<std::string>(attr.second)) {
+								auto strval = nostd::get<std::string>(attr.second);
+								lua_pushstring(L, strval.c_str()); // "stringValue" field value of "value" map
+								lua_setfield(L, -2, "stringValue"); // push "stringValue" field to "value" map
+							}
+
+							lua_setfield(L, -2, "value"); // push "value" field to "attributes" array element
+
+							lua_settable(L, -3); // push "attributes" array element to "attributes" array
+						}
+						lua_setfield(L, -2, "attributes"); // push "attributes" field to "resource" map
+					}
+
+					lua_pushstring(L, std::to_string(metric.start_ts.time_since_epoch().count()).c_str()); // "startTimeUnixNano" field value of "dataPoints" array element
+					lua_setfield(L, -2, "startTimeUnixNano"); // push "startTimeUnixNano" field to "dataPoints" array element
+
+					lua_pushstring(L, std::to_string(metric.end_ts.time_since_epoch().count()).c_str()); // "startTimeUnixNano" field value of "dataPoints" array element
+					lua_setfield(L, -2, "timeUnixNano"); // push "timeUnixNano" field to "dataPoints" array element
+
+					if (nostd::holds_alternative<metric_sdk::SumPointData>(point.point_data)) {
+						auto pt = nostd::get<metric_sdk::SumPointData>(point.point_data);
+						// no int expected here
+						auto val = nostd::get<double>(pt.value_);
+
+						lua_pushnumber(L, val); // "asDouble" field value of "dataPoints" array element
+						lua_setfield(L, -2, "asDouble"); // push "asDouble" field to "dataPoints" array element
+					}
+
+					lua_settable(L, -3); // push "dataPoints" array element to "dataPoints" array
+				}
+				lua_setfield(L, -2, "dataPoints"); // push "dataPoints" field to "scopeMetrics" array element
+
+				lua_setfield(L, -2, "sum"); // push "sum" field of "metrics" array element
+
+				lua_settable(L, -3); // push "metrics" array element to "scopeMetrics" array
+			}
+			lua_setfield(L, -2, "metrics"); // push "metrics" field to "scopeMetrics" array element
+
+			lua_settable(L, -3); // push "scopeMetrics" array element to "scopeMetrics" array
+		}
+
+		lua_setfield(L, -2, "scopeMetrics"); // push "scopeMetrics" field to "resourceMetrics" array element
+
+		lua_settable(L, -3); // push "resourceMetrics" array element to "resourceMetrics" array
+
+		lua_setfield(L, -2, "resourceMetrics"); // push "resourceMetrics" field to return value table
+
+	    return true;
+	});
+
+	return 1;
 }
 
 static int lua_provider_new_meter(struct lua_State *L)
@@ -241,6 +466,7 @@ extern "C" int luaopen_metrics(struct lua_State *L)
 	static const struct luaL_Reg otlp_provider_methods [] = {
 		{"init_otlp_http_exporter", lua_provider_init_otlp_http_exporter},
 		{"meter", lua_provider_new_meter},
+		{"reader", lua_provider_new_reader},
 		{"__gc", lua_clean_provider},
 		{NULL, NULL}
 	};
@@ -276,6 +502,19 @@ extern "C" int luaopen_metrics(struct lua_State *L)
 	setfuncs(L, otlp_double_counter_methods, 0);
 	lua_setfield(L, -2, "__index");
 	lua_pushstring(L, otlp_double_counter_mt_name);
+	lua_setfield(L, -2, "__metatable");
+	lua_pop(L, 1);
+
+	lua_newtable(L);
+	static const struct luaL_Reg otlp_reader_methods [] = {
+		{"collect", lua_reader_collect},
+		{NULL, NULL}
+	};
+	luaL_newmetatable(L, otlp_reader_mt_name);
+	lua_pushvalue(L, -1);
+	setfuncs(L, otlp_reader_methods, 0);
+	lua_setfield(L, -2, "__index");
+	lua_pushstring(L, otlp_reader_mt_name);
 	lua_setfield(L, -2, "__metatable");
 	lua_pop(L, 1);
 
